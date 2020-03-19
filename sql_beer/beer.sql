37217a6b62779486fb9716ac29229e7d640f48f9
Thomas <thomasoctave@yahoo.com>
2020-03-19 09:40:38 +0100

    Question 2 et 3

--- sql_beer/beer.sql
+++ sql_beer/beer.sql
@@ -4,4 +4,15 @@ FROM ventes
 INNER JOIN article ON ventes.ID_ARTICLE = article.ID_ARTICLE 
 WHERE article.ID_ARTICLE = 500;
 
+# 2. Afficher les tickets du 15/01/2014.
+SELECT * 
+FROM beer.ticket
+WHERE DATE_VENTE LIKE ("2014-01-15%");
+
+# 3. Afficher les tickets émis du 15/01/2014 et le 17/01/2014.
+SELECT * 
+FROM beer.ticket
+WHERE DATE_VENTE BETWEEN "2014-01-15 00:00:00" AND "2014-01-18 00:00:00";
+
+
 
